4e39c5bc81ee4433253a610884e4f0c6eb72ecfc
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-08-11 15:45:10 +0300

    paginator bug fixed

--- FINAL_EXAM/common/views.py
+++ FINAL_EXAM/common/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views import generic as views
@@ -12,22 +12,68 @@ from FINAL_EXAM.drawings.models import Drawing
 from FINAL_EXAM.kids.models import Kid
 
 UserModel = get_user_model()
-def show_home_page(request):
-    paginate_by = 3
-    queryset = Drawing.objects.all()
-    search_form = SearchForm()
 
-    page_number = request.GET.get('page', 1)
-    search_query = request.GET.get('drawings_of_searched_kid')
+
+# def show_home_page(request):
+#     paginate_by = 3
+#     queryset = Drawing.objects.all().order_by('-id')
+#     search_form = SearchForm()
+#
+#     page_number = request.GET.get('page', 1)
+#     search_query = request.GET.get('drawings_of_searched_kid')
+#     user = request.user
+#
+#     liked_drawings_by_user = [l.to_drawing_id for l in user.like_set.all()] if user.is_authenticated else []
+#
+#     if search_query:
+#         queryset = queryset.filter(kid_owner_drawing__name__icontains=search_query).all()
+#
+#     paginator = Paginator(queryset, paginate_by)
+#     try:
+#         page_obj = paginator.get_page(page_number)
+#     except PageNotAnInteger:
+#         page_obj = paginator.page(1)
+#     except EmptyPage:
+#         page_obj = paginator.page(paginator.num_pages)
+#
+#     kid_added = search_query in [x.name for x in Kid.objects.all()] if Kid.objects.all() else []
+#
+#     context = {
+#         'kid_added': kid_added,
+#         'drawings': page_obj,
+#         'search_form': search_form,
+#         'page_obj': page_obj,
+#         'liked_drawings_by_user': liked_drawings_by_user,
+#
+#     }
+#     return render(request, 'common/index.html', context)
+
+def show_home_page(request):
     user = request.user
+    queryset = Drawing.objects.all().order_by('-id')
+    search_form = SearchForm()
 
     liked_drawings_by_user = [l.to_drawing_id for l in user.like_set.all()] if user.is_authenticated else []
 
-    if search_query:
-        queryset = queryset.filter(kid_owner_drawing__name__icontains=search_query).all()
+    search_query = request.GET.get('drawings_of_searched_kid')
 
+    if request.method == 'POST':
+        search_form = SearchForm(request.POST)
+        if search_form.is_valid():
+            queryset = queryset.filter(kid_owner_drawing__name__icontains=search_query).all()
+
+    paginate_by = 3
     paginator = Paginator(queryset, paginate_by)
-    page_obj = paginator.get_page(page_number)
+    page_number = request.GET.get('page', 1)
+
+
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
     kid_added = search_query in [x.name for x in Kid.objects.all()] if Kid.objects.all() else []
 
     context = {
@@ -39,8 +85,6 @@ def show_home_page(request):
 
     }
     return render(request, 'common/index.html', context)
-
-
 @login_required
 def like_functionality(request, drawing_id):
     drawing = Drawing.objects.get(id=drawing_id)
