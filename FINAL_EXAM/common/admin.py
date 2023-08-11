4d4348d333678d1807faa4c124822466b4254571
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-08-11 22:27:50 +0300

    refactor in show_home_page

--- FINAL_EXAM/common/admin.py
+++ FINAL_EXAM/common/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from FINAL_EXAM.common.models import Like, Comment
 
 
-# Register your models here.
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'to_drawing')
     list_per_page = 20
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
